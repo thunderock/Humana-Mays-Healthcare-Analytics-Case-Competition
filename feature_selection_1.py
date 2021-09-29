b6ebd6528d4fce45c378295175340a169bd69aac
Ashutosh Tiwari <ashutiwa@iu.edu>
2021-09-29 11:32:24 -0400

    temp

--- feature_selection_1.py
+++ feature_selection_1.py
@@ -1,5 +1,5 @@
 from sklearn.feature_selection import f_classif
-from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import SelectKBest, mutual_info_classif
 from sklearn.svm import SVC
 from sklearn.decomposition import PCA
 import warnings
@@ -21,18 +21,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import joblib
-# import swifter
+from functools import partial
+import lightgbm as lgb
+
 gc.enable()
 warnings.filterwarnings('ignore')
 sns.set(rc={'figure.figsize': (20, 20)})
 student_id = 2000728661
 
 target = "covid_vaccination"
-df = pd.read_csv("dataset/transformed_dataset.csv", low_memory=False)
+df = pd.read_csv("dataset/transformed_dataset.csv", low_memory=False).sample(10000)
 reg_cols, cat_cols = [], []
 id = "ID"
 
-for i in df.columns:
+for idx, i in enumerate(df.columns):
 	if i not in [id, target]:
 		if df[i].nunique() > 100:
 			reg_cols.append(i)
@@ -48,25 +50,30 @@ training_cols = reg_cols + cat_cols
 X, y = df[training_cols], df[target]
 
 pca = PCA(n_components=50, random_state=student_id)
-fs = SelectKBest(score_func=f_classif, k=100)
+cat_index = [i for i in range(len(reg_cols), len(training_cols))]
+score_func = partial(mutual_info_classif, discrete_features=cat_index)
+fs = SelectKBest(score_func=score_func, k=150)
 
 
-combined_features = FeatureUnion([('univ_select', fs), ('pca', pca)], n_jobs=4)
+combined_features = FeatureUnion([('univ_select', fs)], n_jobs=4)
 
 X_features = combined_features.fit(X, y).transform(X)
 print("features in: {}".format(combined_features.n_features_in_))
 print("Combined space has", X_features.shape[1], "features")
 
-tree = RandomForestClassifier(n_jobs=1, random_state=student_id, max_depth=15)
+tree = lgb.LGBMClassier(n_jobs=2, random_seed=student_id, deterministic=True, #device_type='GPU',
+                        max_depth=15, learning_rate=.1)
 
-selection_pipeline = Pipeline([('features', combined_features), ('tree', tree)])
+selection_pipeline = Pipeline(
+	[('features', combined_features), ('tree', tree)])
 
 params = dict(
-    features__pca__n_components=[50],
-    features__univ_select__k=[100],
-    tree__n_estimators=[700, 600])
+    # features__pca__n_components=[50],
+    features__univ_select__k=[150],
+    tree__num_leaves=[700, 600])
 
-search = GridSearchCV(selection_pipeline, param_grid=params, verbose=2, n_jobs=2, cv=3, scoring='roc_auc')
+search = GridSearchCV(selection_pipeline, param_grid=params,
+                      verbose=2, n_jobs=2, cv=3, scoring='roc_auc')
 search.fit(X, y)
 
 print("-------")
@@ -83,4 +90,3 @@ print("-------")
 
 joblib.dump(search, 'grid_search_best_tree.pkl')
 # joblib.load("model_file_name.pkl")
-
